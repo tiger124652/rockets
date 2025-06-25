69ccde558aa9e81901827f437276f6b9162276cb
tiger124652 <tiger124652@gmail.com>
2025-06-25 15:52:34 +0300

    I added a primitive player and gave it the ability to move

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import logging
 import pygame as py
+from settings import WIDTH, HEIGHT
 from player import Player
 
 
@@ -6,11 +8,43 @@ class Game:
     def __init__(self):
         py.init()
 
+        self.screen = py.display.set_mode((WIDTH, HEIGHT))
+        self.clock = py.time.Clock()
+        self.is_running = True
+
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format = "[%(asctime)s] #%(levelname)-8s %(filename)s:"
+            "%(lineno)d - %(name)s - %(message)s"
+            )
+        self.logger = logging.getLogger(__name__)
+
+        self.player = Player(self, WIDTH//2, HEIGHT//2)
+
+        self.logger.info("Игра инициализирована")
+
     def update(self):
-        pass
+        self.player.update()
 
     def handle_events(self):
-        pass
+        for event in py.event.get():
+            if event.type == py.QUIT:
+                self.is_running = False
+            self.player.handle_event(event)
+
+    def render(self):
+        self.screen.fill("black")
+
+        self.player.show()
+
+        py.display.flip()
 
     def start(self):
-        pass
+        while self.is_running:
+            self.handle_events()
+            self.render()
+            self.update()
+
+if __name__ == "__main__":
+    game = Game()
+    game.start()
--- player.py
+++ player.py
@@ -1,15 +1,69 @@
+import logging
 import pygame as py
+from settings import PLAYER_HEALTH, PLAYER_SPEED, WIDTH, HEIGHT
 
 
 class Player:
-    def __init__(self, app):
-        pass
+    def __init__(self, app, x, y):
+        self.app = app
 
-    def update(self):
-        pass
+        self.health = PLAYER_HEALTH
+        self.speed = PLAYER_SPEED
+
+        self.image = py.Surface((100, 100))
+        self.image.fill("red")
+
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+        self.init_flags()
+
+        self.logger = logging.getLogger(__name__)
+        self.logger.info("Player инициализирован")
+
+    def init_flags(self):
+        self.up = False
+        self.down = False
+        self.left = False
+        self.right = False
+
+    def handle_event(self, event):
+        if event.type == py.KEYDOWN:
+            if event.unicode == "w":
+                self.up = True
+            elif event.unicode == "s":
+                self.down = True
+            elif event.unicode == "a":
+                self.left = True
+            elif event.unicode == "d":
+                self.right = True
+        elif event.type == py.KEYUP:
+            if event.unicode == "w":
+                self.up = False
+            elif event.unicode == "s":
+                self.down = False
+            elif event.unicode == "a":
+                self.left = False
+            elif event.unicode == "d":
+                self.right = False
 
     def move(self):
-        pass
+        if self.up and self.rect.top > 0:
+            self.rect = self.rect.move((0, -self.speed))
+        if self.down and self.rect.bottom < HEIGHT:
+            self.rect = self.rect.move((0, self.speed))
+        if self.left and self.rect.left > 0:
+            self.rect = self.rect.move((-self.speed, 0))
+        if self.right and self.rect.right < WIDTH:
+            self.rect = self.rect.move((self.speed, 0))
+
+    def update(self):
+        self.move()
+
+        if self.health <= 0:
+            self.app.is_running = False
+            self.logger.info("Player умер")
 
     def show(self):
-        pass
+        self.app.screen.blit(self.image, self.rect)
